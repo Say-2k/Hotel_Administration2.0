36c0183e98bb4716b2a9d074be7c631d69ab8744
Алексей Кравцов <alexei.cr@mail.ru>
2021-06-04 21:01:03 +0300

    Add page "postklients"

--- Hotel_Administration2.0/migrants.xaml.cs
+++ Hotel_Administration2.0/migrants.xaml.cs
@@ -1,28 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Hotel_Administration2._0
 {
-    /// <summary>
-    /// Логика взаимодействия для migrants.xaml
-    /// </summary>
     public partial class migrants : Page
     {
         public migrants()
         {
             InitializeComponent();
+        }
+
+        private void Print_OnClick(object sender, RoutedEventArgs e)
+        {
+            Excel.Application app = new Excel.Application();
+            app.Visible = true;
+            Excel.Workbook book = app.Workbooks.Add();
+            Excel.Worksheet lst = book.Worksheets[1];
+            lst.Name = "Иностранные клиенты";
+            int i = 0, j = 0;
+            for (i = 0; i < migrantsKlienti.Columns.Count; i++)
+            {
+                lst.Cells[1, i + 1] = dataGridView1.Columns[i].HeaderText;
+            }
+            for (i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    lst.Cells[i + 2, j + 1] = dataGridView1[j, i].Value;
+                }
+            }
+            lst.Range[lst.Cells[1, 1], lst.Cells[1, j + 1]].HorizontalAlignment = 3;
+            lst.Cells.Columns.EntireColumn.AutoFit();
+            lst.Range[lst.Cells[1, 1], lst.Cells[i + 1, j]].Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            lst.Rows[1].Insert();
+            lst.Range[lst.Cells[1, 1], lst.Cells[1, j]].Merge();
+            lst.Cells[1, 1] = "Период формирования отчёта с " + dateTimePicker1.Value.ToShortDateString() + " до " + dateTimePicker2.Value.ToShortDateString();
+            lst.Cells[1, 1].HorizontalAlignment = 3;
+        }
+
+        private void Filter(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- Hotel_Administration2.0/postklients.xaml.cs
+++ Hotel_Administration2.0/postklients.xaml.cs
@@ -1,28 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Hotel_Administration2._0
 {
-    /// <summary>
-    /// Логика взаимодействия для postklients.xaml
-    /// </summary>
     public partial class postklients : Page
     {
         public postklients()
         {
             InitializeComponent();
+        }
+
+        private void Print_OnClick(object sender, RoutedEventArgs e)
+        {
+            Excel.Application app = new Excel.Application();
+            app.Visible = true;
+            Excel.Workbook book = app.Workbooks.Add();
+            Excel.Worksheet lst = book.Sheets[1];
+            lst.Name = "Постоянные клиенты";
+            int i = 0, j = 0;
+            for (i = 1; i < postoyannieKlienti.Columns.Count; i++)
+            {
+                lst.Cells[1, i] = postoyannieKlienti.Columns[i].Header;
+            }
+            for (i = 0; i < postoyannieKlienti.Items.Count; i++)
+            {
+                DataRowView klientRow = (DataRowView)postoyannieKlienti.Items[i];
+                for (j = 1; j < postoyannieKlienti.Columns.Count; j++)
+                {
+                    lst.Cells[i + 2, j] = klientRow[j];
+                }
+            }
+            lst.Range[lst.Cells[1, 1], lst.Cells[1, j]].HorizontalAlignment = 3;
+            lst.Cells.Columns.EntireColumn.AutoFit();
+            lst.Range[lst.Cells[1, 1], lst.Cells[i + 1, j - 1]].Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            lst.Rows[1].Insert();
+            lst.Range[lst.Cells[1, 1], lst.Cells[1, j - 1]].Merge();
+            lst.Cells[1, 1] = "Период формирования отчёта с " + dataDogovora_Start.Text + " по " + dataDogovora_End.Text;
+            lst.Cells[1, 1].HorizontalAlignment = 3;
+        }
+
+        private void Filter(object sender, RoutedEventArgs e)
+        {
+            if (dataDogovora_Start.SelectedDate != null && dataDogovora_End.SelectedDate != null) 
+            {
+                Pull(dataDogovora_Start.SelectedDate.Value.ToShortDateString(), dataDogovora_End.SelectedDate.Value.ToShortDateString());
+                postoyannieKlienti.ItemsSource = Connect.Ds.Tables["postklients"].DefaultView;
+            }
+        }
+
+        private void Postklients_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            dataDogovora_Start.Text = dataDogovora_Start.DisplayDate.Date.ToShortDateString();
+            dataDogovora_End.Text = dataDogovora_End.DisplayDate.Date.ToShortDateString();
+            postoyannieKlienti.ItemsSource = Connect.Ds.Tables["postklients"].DefaultView;
+            postoyannieKlienti.Columns[0].Visibility = Visibility.Hidden;
+            postoyannieKlienti.AutoGenerateColumns = true;
+            postoyannieKlienti.CanUserAddRows = false;
+            postoyannieKlienti.HeadersVisibility = DataGridHeadersVisibility.Column;
+            postoyannieKlienti.IsReadOnly = true;
+            postoyannieKlienti.SelectionMode = DataGridSelectionMode.Extended;
+        }
+
+        private void Pull(string date_start, string date_end)
+        {
+            string sql = "SELECT Dogovor.IdKlienta, " +
+                         "CONCAT(Familiya, ' ', Imya, ' ', Otchestvo) AS [ФИО клиента], " +
+                         "BonusnayaKarta AS [Вид бонусной карты], " +
+                         "COUNT(*) AS [Количество договоров за указанный период], " +
+                         "SUM(SummaOplati) AS [Общая сумма оплаты] " +
+                         "FROM Dogovor INNER JOIN Klient ON Dogovor.IdKlienta = Klient.IdKlienta " +
+                         "WHERE DataDogovora >= '" + date_start + "' AND DataDogovora <= '" + date_end + "' " +
+                         "GROUP BY Dogovor.IdKlienta, Familiya, Imya, Otchestvo, BonusnayaKarta " +
+                         "HAVING COUNT(*) >= 3";
+            Connect.Table_Fill("postklients", sql);
         }
     }
 }
